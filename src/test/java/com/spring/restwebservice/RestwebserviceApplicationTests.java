f2c6fd45f18ec57e9662d21394493ba088ad114d
Sokuro Matsuda <ugorcak@gmx.ch>
2021-02-07 19:57:12 +0100

    Restful WebService: added Jackson Datafornat XML

---
+++ src/test/java/com/spring/restwebservice/RestwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
