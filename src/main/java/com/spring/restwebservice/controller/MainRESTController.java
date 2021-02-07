e125273fbe12ed77a3ceae061a5082cb199ee3f1
Sokuro Matsuda <ugorcak@gmx.ch>
2021-02-07 20:56:55 +0100

    Restful WebService: added Controller: Get All Employees Mapping

--- src/main/java/com/spring/restwebservice/controller/MainRESTController.java
+++ src/main/java/com/spring/restwebservice/controller/MainRESTController.java
@@ -1,10 +1,15 @@
 package com.spring.restwebservice.controller;
 
 import com.spring.restwebservice.dao.EmployeeDAO;
+import com.spring.restwebservice.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MainRESTController {
@@ -17,4 +22,20 @@ public class MainRESTController {
     public String welcome() {
         return "Welcome to RestTemplate Example.";
     }
+
+    // URL:
+    // http://localhost:8080/employees
+    // http://localhost:8080/employees.xml
+    // http://localhost:8080/employees.json
+    @RequestMapping(
+            value = "/employees", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public List<Employee> getEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+
 }
